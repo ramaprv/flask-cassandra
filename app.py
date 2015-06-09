efc462945a895bd0449bbf23b0b00bbd94fc17e6
prashv <prashv@icloud.com>
2015-06-09 11:22:38 +0530

    Python Flask Cassandra simple shop CR
    Python Flask Cassandra simple shop CR initial commit

---
+++ app.py
@@ -0,0 +1,84 @@
+import datetime
+import json
+import os
+import time_uuid
+import uuid
+from cassandra.cluster import Cluster
+from flask import Flask, request
+
+def _connect_to_cassandra(keyspace):
+    """
+    Connect to the Cassandra cluster and return the session.
+    """
+
+    if 'CASSANDRA_URL' in os.environ:
+        host = os.environ['CASSANDRA_URL']
+    else:
+        host = 'localhost'
+
+    cluster = Cluster([host])
+    session = cluster.connect(keyspace)
+
+    return session
+
+"""
+Create the Flask application, connect to Cassandra, and then
+set up all the routes.
+"""
+app = Flask(__name__)
+session = _connect_to_cassandra('sensorapp')
+
+@app.route('/shopping_list', methods=['GET'])
+def get_shopping_list():
+    """
+    Fetch shopping_list
+    """
+
+    user_id = str(request.args['user_id'])
+
+
+    query = """SELECT * FROM shopping_list
+               WHERE user_id=%(user_id)s
+               ORDER BY created_at DESC"""
+
+    values = { 'user_id': user_id }
+
+    rows = session.execute(query, values)
+    reply = { 'results' : [] }
+    for r in rows:
+        ts = time_uuid.TimeUUID(str(r.time))
+        dt = str(ts.get_datetime())
+        reply['results'].append({ 'item' : str(r.item),
+                               'quantity': str(r.quantity),
+                               'created_at': str(created_at) })
+    return json.dumps(reply)
+
+@app.route('/shopping_list', methods=['POST'])
+def put_shopping_list():
+    """
+    Insert shopping_list
+    """
+
+    user_id = str(request.args['user_id'])
+    item = str(request.args['item'])
+    quantity = float(request.args['quantity'])
+
+    day = datetime.datetime.utcnow() - datetime.timedelta(days=days)
+    timestamp = time_uuid.TimeUUID.with_utc(day, randomize=False, lowest_val=True)
+
+    query = """INSERT INTO shopping_list
+               (user_id, created_at, item, quantity)
+               VALUES (%(user_id)s, %(created_at)s, %(item)s, %(quantitiy)f)"""
+
+    values = { 'user_id': user_id,
+               'created_at': timestamp,
+               'item': item,
+               'quantity': quantity }
+
+    session.execute(query, values)
+
+    reply = { 'result' : values }
+    return json.dumps(reply)
+
+if __name__ == '__main__':
+    app.run()
---
+++ rest-client.py
@@ -0,0 +1,55 @@
+import json
+import logging
+import requests
+import sys
+
+
+REST_SERVER = 'http://localhost:5000'
+
+
+def post_data():
+    """
+    Post some data to the REST server.
+    """
+
+     user_id = raw_input("user_id : ")
+     item = raw_input("item : ")
+     quantity = raw_input("quantity : ")
+     values = { 'user_id' : user_id,
+                'item' : item,
+                'quantity' : quantity }
+    # Post the data to the webserver.
+    res = requests.post(REST_SERVER + '/shopping_list', data=values)
+    return json.loads(res.text)
+
+
+def fetch_data():
+    """
+    Fetch data from the REST server.
+    """
+    user_id = raw_input("user id: ")
+
+    res = requests.get(REST_SERVER + '/shopping_list', params=user_id)
+    return json.loads(res.text)
+
+
+def main(argv=None):
+    if argv[1] == 'post':
+        data = post_data()
+        print json.dumps(data,
+                         sort_keys=True,
+                         indent=2,
+                         separators=(',',':'))
+    elif argv[1] == 'fetch':
+        data = fetch_data()
+        print json.dumps(data,
+                         sort_keys=True,
+                         indent=2,
+                         separators=(',',':'))
+
+if __name__ == '__main__':
+    # So that we don't get so many random warnings.
+    requests_log = logging.getLogger("requests")
+    requests_log.setLevel(logging.WARNING)
+
+    main(sys.argv)
