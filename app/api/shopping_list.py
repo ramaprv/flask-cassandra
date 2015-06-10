87ea0fe64d3dcc858d5cf0b473d93294d74448fe
prashv <prashv@icloud.com>
2015-06-10 17:32:41 +0530

    status codes guidelines

--- app/api/shopping_list.py
+++ app/api/shopping_list.py
@@ -4,8 +4,7 @@ import time_uuid
 import datetime
 
 from uuid import UUID
-from flask import request
-from flask import app
+from flask import Flask, app, jsonify, abort, request, make_response, url_for
 from app.api import api
 from app.models import ShoppingList
 
@@ -22,6 +21,21 @@ def _validate_uuid4(uuid_string):
         return False
 
     return val.hex == uuid_string
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 403)
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
 
 @api.route('/shopping_list/<id>', methods=['GET'])
@@ -41,9 +55,9 @@ def get_shopping_list(id):
                                      'quantity' : instance.quantity })
             i += 1
             if i == query.count():
-                return str(data)
+                return make_response(jsonify(data), 200)
     else:
-        return json.dumps({'success':True,'results':'Empty Shopping List'})
+        return make_response(jsonify({'success':True,'results':'Empty Shopping List'}), 204)
 
 
 @api.route('/shopping_list', methods=['POST'])
@@ -57,21 +71,21 @@ def post_shopping_list():
     if hasattr(data, 'user_id'):
         user_id = int(data['user_id'])
     else:
-        return json.dumps({'success':False,'result':'User id is empty'})
+        return make_response(jsonify({'success':False,'result':'User id is empty'}), 400)
 
     if hasattr(data, 'item'):
         item = ''.join(str(e) for e in data['item'])
     else:
-        return json.dumps({'success':False,'result':'Item is empty'})
+        return make_response(jsonify({'success':False,'result':'Item is empty'}), 400)
 
     if hasattr(data, 'quantity'):
         quantity = int(data['quantity'])
     else:
-        return json.dumps({'success':False,'result':'Quantity is empty'})
+        return make_response(jsonify({'success':False,'result':'Quantity is empty'}), 400)
 
     ShoppingList.create(user_id=user_id, item=item, quantity=quantity)
 
-    return json.dumps({'success':True,'result':'Shopping List Updated'})
+    return make_response(jsonify({'success':True,'result':'Shopping List Created'}), 201)
 
 
 @api.route('/shopping_list/<id>', methods=['PUT'])
@@ -91,9 +105,9 @@ def update_shopping_list(id):
         if hasattr(data, 'quantity'):
             quantity = int(data['quantity'])
             query.update(quantity=quantity)
-        return json.dumps({'success':True,'result':'Shopping list updated'})
+        return make_response(jsonify({'success':True,'result':'Shopping list updated'}), 201)
     else:
-        return json.dumps({'success':False,'result':'Invalid shopping list id'})
+        return make_response(jsonify({'success':False,'result':'Invalid shopping list id'}), 400)
 
 
 @api.route('/shopping_list/<id>', methods=['DELETE'])
@@ -105,6 +119,6 @@ def delete_shopping_list(id):
     if _validate_uuid4(id):
         query = ShoppingList.get(id=id)
         query.delete()
-        return json.dumps({'success':True,'result':'Shopping list deleted'})
+        return make_response(jsonify({'success':True,'result':'Shopping list deleted'}), 204)
     else:
-        return json.dumps({'success':False,'result':'Invalid shopping list id'})
+        return make_response(jsonify({'success':False,'result':'Invalid shopping list id'}), 400)
